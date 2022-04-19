992b8a59be92c394e92897f5b30936a4b0b929d0
Kareem ElFaramawi <kareem02@gmail.com>
2022-04-19 09:52:11 -0400

    typescript rewrite, works with latest youtube update

---
+++ src/disableAutoplay.ts
@@ -0,0 +1,36 @@
+import $ from 'jquery';
+
+async function waitForElement(selector: string): Promise<JQuery<HTMLElement>> {
+    return new Promise(resolve => {
+        if ($(selector).length) {
+            return resolve($(selector));
+        }
+
+        const observer = new MutationObserver(() => {
+            if ($(selector).length) {
+                resolve($(selector));
+                observer.disconnect();
+            }
+        });
+
+        observer.observe(document.body, {
+            childList: true,
+            subtree: true
+        });
+    });
+}
+
+async function disableAutoplay() {
+    const autoplayToggle = await waitForElement('div.ytp-right-controls > button[data-tooltip-target-id="ytp-autonav-toggle-button"]');
+
+    // If autoplay is currently enabled, click the toggle
+    if (autoplayToggle.attr('aria-label')?.search(/is on/gi) !== -1) {
+        autoplayToggle.trigger('click');
+        setTimeout(disableAutoplay, 100);  // Keep retrying until we're sure it's off
+    }
+}
+
+$(() => {
+    disableAutoplay();
+    $(document).on('spfdone yt-navigate-finish viewport-load', disableAutoplay);
+});
---
+++ webpack/webpack.common.js
@@ -0,0 +1,32 @@
+const webpack = require("webpack");
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
+const srcDir = path.join(__dirname, "..", "src");
+
+module.exports = {
+    entry: {
+      disableAutoplay: path.join(srcDir, 'disableAutoplay.ts'),
+    },
+    output: {
+        path: path.join(__dirname, "../dist/js"),
+        filename: "[name].js",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: "ts-loader",
+                exclude: /node_modules/,
+            },
+        ],
+    },
+    resolve: {
+        extensions: [".ts", ".tsx", ".js"],
+    },
+    plugins: [
+        new CopyPlugin({
+            patterns: [{ from: ".", to: "../", context: "public" }],
+            options: {},
+        }),
+    ],
+};
